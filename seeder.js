71b4f5df8395123a2a942f2c3beccf0c0054464c
nguyentruong <truongaht00@gmail.com>
2020-11-26 22:24:01 +0700

    v

--- seeder.js
+++ seeder.js
@@ -12,46 +12,46 @@ const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // Read Json file
 const bootcamps = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 const courses = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
 // Import into DB
-const importData = async() => {
-    try {
-        await Bootcamp.create(bootcamps);
-        await Course.create(courses);
-        console.log("Data Imported...".green.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    // await Course.create(courses);
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Delete Data
 
-const deleteData = async() => {
-    try {
-        await Bootcamp.deleteMany();
-        await Course.deleteMany();
-        console.log("Data Destroyed...".red.inverse);
-        process.exit();
-    } catch (err) {
-        console.log(err);
-    }
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 if (process.argv[2] === "-i") {
-    importData();
+  importData();
 } else if (process.argv[2] === "-d") {
-    deleteData();
+  deleteData();
 }
